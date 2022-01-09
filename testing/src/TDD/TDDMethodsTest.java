598b15e49fd3a5320cec045f4373ded7fcaad230
Veronika Kustermann <mail@vk-art.de>
2022-01-09 15:34:34 +0100

    ESA 10: Fix typos

--- testing/src/TDD/TDDMethodsTest.java
+++ testing/src/TDD/TDDMethodsTest.java
@@ -13,31 +13,31 @@ class TDDMethodsTest {
     }
 
     @org.junit.jupiter.api.Test
-    @DisplayName("Test fuction changeToEur successfully")
+    @DisplayName("Test function changeToEur successfully")
     void changeToEurSuccessfully() {
         assertEquals("I have 500 €, you have 800 €.", tddMethods.changeToEur("Euro", "I have 500 Euro, you have 800 Euro."));
     }
 
     @org.junit.jupiter.api.Test
-    @DisplayName("Test fuction changeToEur without occurence of echange parameter")
+    @DisplayName("Test function changeToEur without occurence of exchange parameter")
     void changeToEurWithoutOccurence() {
         assertEquals("I have 500 Dollars, you have 800 Dollars.", tddMethods.changeToEur("Euro", "I have 500 Dollars, you have 800 Dollars."));
     }
 
     @org.junit.jupiter.api.Test
-    @DisplayName("Test fuction changeToEur with empty exhange paramter")
+    @DisplayName("Test function changeToEur with empty exhange parameter")
     void changeToEurWithEmptyStringToExchange() {
         assertEquals("I have 500 Dollars, you have 800 Dollars.", tddMethods.changeToEur("", "I have 500 Dollars, you have 800 Dollars."));
     }
 
     @org.junit.jupiter.api.Test
-    @DisplayName("Test fuction changeToEur with empty text paramter")
+    @DisplayName("Test function changeToEur with empty text parameter")
     void changeToEurWithEmptyText() {
         assertEquals("", tddMethods.changeToEur("Euro", ""));
     }
 
     @org.junit.jupiter.api.Test
-    @DisplayName("Test fuction changeToEur with parameter null")
+    @DisplayName("Test function changeToEur with parameter null")
     void loginException() {
         assertThrows(IllegalArgumentException.class,
                 () -> tddMethods.changeToEur(null, "I have 500 Euro, you have 800 Euro."),
@@ -49,25 +49,25 @@ class TDDMethodsTest {
     }
 
     @org.junit.jupiter.api.Test
-    @DisplayName("Test fuction countImages successfully")
+    @DisplayName("Test function countImages successfully")
     void countImagesSuccessfully() {
         assertEquals(5, tddMethods.countImages("<image> This is a headline. <image> Some content. <image><image> Some more content. <image>"));
     }
 
     @org.junit.jupiter.api.Test
-    @DisplayName("Test fuction countImages when no image exists")
+    @DisplayName("Test function countImages when no image exists")
     void countImagesWithoutImages() {
         assertEquals(0, tddMethods.countImages("This is a headline. Some content. Some more content."));
     }
 
     @org.junit.jupiter.api.Test
-    @DisplayName("Test fuction countImages with empty text")
+    @DisplayName("Test function countImages with empty text")
     void countImagesWithEmptyText() {
         assertEquals(0, tddMethods.countImages(""));
     }
 
     @org.junit.jupiter.api.Test
-    @DisplayName("Test fuction countImages with null text")
+    @DisplayName("Test function countImages with null text")
     void countImagesWithNulText() {
         assertThrows(IllegalArgumentException.class, () -> tddMethods.countImages(null), "Text must not be null.");
     }
