b0a7194fe86d5cb5d144cc52cfd7a216c109fb6c
Veronika Kustermann <mail@vk-art.de>
2022-01-09 10:21:16 +0100

    ESA 10: TDD Images round 6

--- testing/src/TDD/TDDMethods.java
+++ testing/src/TDD/TDDMethods.java
@@ -21,7 +21,18 @@ public class TDDMethods {
         return text.replaceAll(stringToExchange, "€");
     }
 
-    public int countImages(String text) {
+    /**
+     * Returns the amount of "<image>" strings in a text.
+     *
+     * @param text text to search for images in
+     * @return the amount of images in the text
+     * @throws IllegalArgumentException if text is null
+     */
+    public int countImages(String text) throws IllegalArgumentException {
+        if (text == null) {
+            throw new IllegalArgumentException("Text must not be null.");
+        }
+
         int lengthImage = "<image>".length();
         int count = 0;
         int imageIndex = text.indexOf("<image>");
