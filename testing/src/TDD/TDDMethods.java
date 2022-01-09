07cfa7289947eac3c38af2b3eae9e12f32f35f8f
Veronika Kustermann <mail@vk-art.de>
2022-01-09 09:46:30 +0100

    ESA 10: TDD Round 7

--- testing/src/TDD/TDDMethods.java
+++ testing/src/TDD/TDDMethods.java
@@ -1,7 +1,19 @@
 package TDD;
 
 public class TDDMethods {
-    public String changeToEur(String stringToExchange, String text) {
+    /**
+     * Method to replace any string with €.
+     *
+     * @param stringToExchange string that should be replaced with €
+     * @param text text that should be searched
+     * @return the text with replaced strings
+     * @throws IllegalArgumentException if any parameter is null
+     */
+    public String changeToEur(String stringToExchange, String text) throws IllegalArgumentException {
+        if (stringToExchange == null || text == null) {
+            throw new IllegalArgumentException("Parameters must not be null.");
+        }
+
         if (stringToExchange.equals("")) {
             return text;
         }
